362c061f9f9a961cd5009877fa688b3353eb4363
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2023-04-05 16:29:53 +0100

    Adding the dashboard

--- src/App.js
+++ src/App.js
@@ -39,11 +39,11 @@ const App = () => {
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
               <Navbar />
             </div>
-          </div>
+          
           <div>
             <Routes>
               {/* Dashboard */}
-              {/* <Route path='/' element={(<Ecommerce />)} /> */}
+              <Route path='/' element={(<Ecommerce />)} />
               <Route path='/ecommerce' element={(<Ecommerce />)} />
 
               {/* Pages */}
@@ -69,6 +69,7 @@ const App = () => {
 
 
             </Routes>
+          </div>
           </div>
         </div>
       </BrowserRouter>
