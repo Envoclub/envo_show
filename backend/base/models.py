0093b23a8b07fab1abd0e10cd9dbae8fbc665f10
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2023-04-06 22:47:34 +0100

    Completing backend

---
+++ backend/base/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+class UserAccountManager(BaseUserManager):
+    '''Custom Account manager for managing roles'''
+    ordering = ('email',)
+
+    def create_user(self, email, first_name, last_name, password, **other_fields):
+        '''Defining and creating user function'''
+        if not email:
+            raise ValueError(_("Email Is Mandatory"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password, **other_fields):
+        '''Defining and creating superuser function'''
+        other_fields.setdefault("is_staff", True)
+        other_fields.setdefault("is_superuser", True)
+        other_fields.setdefault("is_active", True)
+        other_fields.setdefault("is_staff", True)
+        return self.create_user(email, first_name, last_name, password, **other_fields)
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        return self.first_name
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def __str__(self):
+        return self.email
